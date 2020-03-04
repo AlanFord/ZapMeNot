df28fd129914d4162536f2cc6767498e46abac0d
Alan Ford <alanford354@gmail.com>
2020-03-04 12:11:30 -0500

    restructuring file tree to comply with best practices for packages and pyttest

--- tests/test_isotope.py
+++ tests/test_isotope.py
@@ -1,40 +1,39 @@
-import unittest
+import pytest
 from ZapMeNot import isotope
 
 
-class testIsotope(unittest.TestCase):
-	# test response to a bad isotope name
-	def test_a_bad_material_name(self):
-		self.assertRaises(ValueError, isotope.Isotope, "wanker")
+def test_a_bad_material_name():
+	with pytest.raises(ValueError):
+		a = isotope.Isotope("wanker")
 
-	# test valid half life in years
-	def test_halflifeYears(self):
-		a = isotope.Isotope("co-60")
-		self.assertAlmostEqual(a.half_life, 5.271*365.25*24*60*60)
+# test valid half life in years
+def test_halflifeYears():
+	a = isotope.Isotope("co-60")
+	assert  a.half_life == pytest.approx(5.271*365.25*24*60*60)
 
-	# test valid half life in days
+# test valid half life in days
 
-	# test valid half life in hours
-	def test_halflifeMinutes(self):
-		a = isotope.Isotope("cu-67")
-		self.assertAlmostEqual(a.half_life, 61.86*60*60)
+# test valid half life in hours
+def test_halflifeMinutes():
+	a = isotope.Isotope("cu-67")
+	assert a.half_life == pytest.approx(61.86*60*60)
 
-	# test valid half life in minutes
-	def test_halflifeMinutes(self):
-		a = isotope.Isotope("c-11")
-		self.assertAlmostEqual(a.half_life, 20.38*60)
+# test valid half life in minutes
+def test_halflifeMinutes():
+	a = isotope.Isotope("c-11")
+	assert a.half_life == pytest.approx(20.38*60)
 
-	# test valid half life in seconds
+# test valid half life in seconds
 
-	# test valid photon property read and store
-	def test_photon_readAndStore(self):
-		a = isotope.Isotope("co-60")
-		self.assertEqual(a.photons, [[3.46930e-01, 7.59999e-05], \
-  									 [8.26280e-01, 7.59999e-05], \
-  									 [1.17321e+00, 9.99000e-01], \
-  									 [1.33247e+00, 9.99824e-01], \
-  									 [2.15877e+00, 1.10000e-05], \
-  									 [2.50500e+00, 3.60000e-08]])
+# test valid photon property read and store
+def test_photon_readAndStore():
+	a = isotope.Isotope("co-60")
+	assert a.photons ==         [[3.46930e-01, 7.59999e-05], \
+									 [8.26280e-01, 7.59999e-05], \
+									 [1.17321e+00, 9.99000e-01], \
+									 [1.33247e+00, 9.99824e-01], \
+									 [2.15877e+00, 1.10000e-05], \
+									 [2.50500e+00, 3.60000e-08]]
 
 
 
