b7180aa0105bdef2264883de1316a40c1f380edd
Alan Ford <alanford354@gmail.com>
2020-03-14 08:25:41 -0400

    removed last references to unittest

--- tests/test_material.py
+++ tests/test_material.py
@@ -1,41 +1,43 @@
-import unittest
 from ZapMeNot import material
+import pytest
 
-class testMaterial(unittest.TestCase):
-	# test response to a bad material name
-	def test_a_bad_material_name(self):
-		self.assertRaises(ValueError, material.Material, "wanker")
+# test response to a bad material name
+def test_a_bad_material_name():
+	with pytest.raises(ValueError):
+		a = material.Material("wanker")
 
-	# test setting a new density
-	def test_setDensity(self):
-		a = material.Material("air")
-		a.setDensity(3.14)
-		self.assertEqual(a.density, 3.14)
+# test setting a new density
+def test_setDensity():
+	a = material.Material("air")
+	a.setDensity(3.14)
+	assert a.density == 3.14
 
-	# test retrieval of the MFP
-	def test_getMfp(self):
-		a = material.Material("air")
-		b = a.getMfp(0.66, 10)
-		self.assertAlmostEqual(b, 9.282790078911769e-04)
+# test retrieval of the MFP
+def test_getMfp():
+	a = material.Material("air")
+	b = a.getMfp(0.66, 10)
+	assert b == pytest.approx(9.282790078911769e-04)
 
-	# test retrieval of a mass attenuation coefficient
-	# WARNING: result is based on preliminary test dataset!
-	# comparison value calculated in Matlab
-	def test_getMassAttenCoff(self):
-		a = material.Material("air")
-		b = a.getMassAttenCoff(0.66)
-		self.assertAlmostEqual(b,0.077035602314621)
+# test retrieval of a mass attenuation coefficient
+# WARNING: result is based on preliminary test dataset!
+# comparison value calculated in Matlab
+def test_getMassAttenCoff():
+	a = material.Material("air")
+	b = a.getMassAttenCoff(0.66)
+	assert b == pytest.approx(0.077035602314621)
 
-	# test response to a bad material name
-	def test_a_bad_buildupFactorType(self):
-		a = material.Material("air")
-		self.assertRaises(ValueError, a.getBuildupFactor, 0.66, 10, "Taylor")
+# test response to a bad material name
+def test_a_bad_buildupFactorType():
+	a = material.Material("air")
+	#cassertRaises(ValueError, a.getBuildupFactor, 0.66, 10, "Taylor")
+	with pytest.raises(ValueError):
+		b = a.getBuildupFactor(0.66, 10, "Taylor")
 
-	# test calculation of a buildup factor
-	def test_getBuildupFactor(self):
-		a = material.Material("air")
-		b = a.getBuildupFactor(0.66, 10, "GP")
-		self.assertAlmostEqual(b,43.23778738585646)
+# test calculation of a buildup factor
+def test_getBuildupFactor():
+	a = material.Material("air")
+	b = a.getBuildupFactor(0.66, 10, "GP")
+	assert b == pytest.approx(43.23778738585646)
 
 
 
--- tests/test_shield.py
+++ tests/test_shield.py
@@ -1,43 +1,45 @@
-import unittest
+import pytest
 from ZapMeNot import shield, ray, material
 
-class testXInfiniteSlab(unittest.TestCase):
+class testYAlignedCylinder():
 
-	def setUp(self):
+	@pytest.fixture(scope="class")
+	def create_shield(self):
+		self.myShield = shield.YAlignedCylinder(materialName="iron", cylinderCenter=[0,0,0],cylinderLength=100,cylinderRadius=10)
+		start = [-10,20,0]
+		end = [30,20,0]
+		self.aRay = ray.Ray(start, end)
+
+	def test_crossing_length(self, create_shield):
+		length = self.myShield.getCrossingLength(self.aRay)
+		assert length == pytest.approx(20)
+
+class testXInfiniteSlab():
+
+	@pytest.fixture(scope="class")
+	def create_shield(self):
 		self.myShield = shield.SemiInfiniteXSlab("iron", 10, 20)
 		start = [0,0,0]
 		end = [30,30,30]
 		self.aRay = ray.Ray(start, end)
 
 	# test getting a crossing length
-	def test_crossing_length(self):
+	def test_crossing_length(self, create_shield):
 		length = self.myShield.getCrossingLength(self.aRay)
-		self.assertAlmostEqual(length,17.320508075688775)
+		assert length == pytest.approx(17.320508075688775)
 
 	# test getting a crossing mfp
-	def test_get_MFP(self):
+	def test_get_MFP(self, create_shield):
 		mfp = self.myShield.getCrossingMFP(self.aRay, 0.66)
-		self.assertAlmostEqual(mfp,9.923087573149688)
+		assert mfp == pytest.approx(9.923087573149688)
 
 	# test getting a crossing mfp
-	def test_get_special_MFP(self):
+	def test_get_special_MFP(self, create_shield):
 		start = [0,0,0]
 		end = [100,0,0]
 		self.aRay = ray.Ray(start, end)
 		mfp = self.myShield.getCrossingMFP(self.aRay, 1)
-		self.assertAlmostEqual(mfp,4.6905418)
-
-class TestYAlignedCylinder(unittest.TestCase):
-
-	def setUp(self):
-		self.myShield = shield.YAlignedCylinder(materialName="iron", cylinderCenter=[0,0,0],cylinderLength=100,cylinderRadius=10)
-		start = [-10,20,0]
-		end = [30,20,0]
-		self.aRay = ray.Ray(start, end)
-
-	def test_crossing_length(self):
-		length = self.myShield.getCrossingLength(self.aRay)
-		self.assertAlmostEqual(length,20)
+		assert mfp == pytest.approx(4.6905418)
 
 
 
