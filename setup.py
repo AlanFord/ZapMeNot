0b93c3b2722740f16d77b2bf97f003161efb0307
Alan Ford <alanford354@gmail.com>
2020-05-12 16:12:26 -0400

    first cut at packaging

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# To prevent importing about and thereby breaking the coverage info we use this
+# exec hack
+about = {}
+with open('zap_me_not/__about__.py') as fh:
+    exec(fh.read(), about)
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name=about['__package_name__'],
+        version=about['__version__'],
+        author=about['__author__'],
+        author_email=about['__author_email__'],
+        description=about['__description__'],
+        url=about['__url__'],
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3"
+        ],
+        python_requires='>=3.4',
+        intall_requires=[
+            'numpy>=1.18.1',
+            'scipy>=0.14',
+            'pyyaml>=5.3'],
+        test_requires=['pytest'],
+        setup_requires=['pytest_runner']
+    )
