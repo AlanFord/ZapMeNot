correct calculation of source point weights

tests/test_source.py
@@ -163,7 +163,7 @@ class TestPointSource():
         assert create_source._x == 1
         assert create_source._y == 2
         assert create_source._z == 3
-        assert create_source.points_per_dimension == [1.]
+        assert create_source.points_per_dimension == [1, 1, 1]
         # test attribute of shield class
         assert create_source.material.name == "air"
         # test attribute of source class
@@ -198,7 +198,7 @@ class TestLineSource():
         assert create_source._length == pytest.approx(10 * np.sqrt(3))
         single = np.sqrt(1./3.)
         assert all(create_source._dir == [single, single, single])
-        assert create_source.points_per_dimension == [10]
+        assert create_source.points_per_dimension == [10, 1, 1]
         # test attribute of shield class
         assert create_source.material.name == "air"
         # test attribute of source class
@@ -221,8 +221,7 @@ class TestLineSource():
 
     def test_getSourcePointWeights(self, create_source):
         number_of_points = create_source.points_per_dimension
-        assert create_source._get_source_point_weights() == \
-            [1.0 / np.product(number_of_points)] * np.sum(number_of_points)
+        assert create_source._get_source_point_weights() == [1.0 / 10] * 10
 
     def test_infinite(self, create_source):
         assert create_source.is_infinite() is False
@@ -278,8 +277,7 @@ class TestBoxSource():
 
     def test_getSourcePointWeights(self, create_source):
         number_of_points = create_source.points_per_dimension
-        the_list = [1.0 / np.product(number_of_points)] * \
-            np.sum(number_of_points)
+        the_list = [1.0 / 1000] * 1000
         assert create_source._get_source_point_weights() == the_list
 
 # =============================================================
@@ -345,8 +343,7 @@ class TestXAlignedCylinderSource():
 
     def test_getSourcePointWeights(self, create_source):
         number_of_points = create_source.points_per_dimension
-        the_list = [1.0 / np.product(number_of_points)] * \
-            np.sum(number_of_points)
+        the_list = [1.0 / 27] * 27
         assert create_source._get_source_point_weights() == the_list
 
 # =============================================================
@@ -412,8 +409,7 @@ class TestYAlignedCylinderSource():
 
     def test_getSourcePointWeights(self, create_source):
         number_of_points = create_source.points_per_dimension
-        the_list = [1.0 / np.product(number_of_points)] * \
-            np.sum(number_of_points)
+        the_list = [1.0 / 27] * 27
         assert create_source._get_source_point_weights() == the_list
 
 # =============================================================
@@ -479,6 +475,5 @@ class TestZAlignedCylinderSource():
 
     def test_getSourcePointWeights(self, create_source):
         number_of_points = create_source.points_per_dimension
-        the_list = [1.0 / np.product(number_of_points)] * \
-            np.sum(number_of_points)
+        the_list = [1.0 / 27] * 27
         assert create_source._get_source_point_weights() == the_list

zap_me_not/model.py
@@ -216,8 +216,7 @@ class Model:
                         photon_energy, total_mfp)
             else:
                 buildup_factor = 1.0
-
-            uncollided_point_energy_flux = photon_yield * source_point_weights\
+            uncollided_point_energy_flux = photon_yield * np.asarray(source_point_weights) \
                 * uncollided_flux_factor * photon_energy * \
                 (1/(4*math.pi*np.power(total_distance, 2)))
             total_uncollided_energy_flux = np.sum(uncollided_point_energy_flux)

zap_me_not/source.py
@@ -285,7 +285,7 @@ class LineSource(Source, shield.Shield):
         super().__init__(**kwargs)
         # initialize points_per_dimension after super() to force a
         # single dimension
-        self.points_per_dimension = [10]
+        self.points_per_dimension = [10, 1, 1]
 
     def is_infinite(self):
         """Returns true if any dimension is infinite, false otherwise
@@ -294,7 +294,7 @@ class LineSource(Source, shield.Shield):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.
@@ -306,7 +306,7 @@ class LineSource(Source, shield.Shield):
         """
         spacings = np.linspace(1, self.points_per_dimension[0],
                                self.points_per_dimension[0])
-        mesh_width = self._length/self.points_per_dimension
+        mesh_width = self._length/self.points_per_dimension[0]
         spacings = spacings*mesh_width
         spacings = spacings-(mesh_width/2)
         source_points = []
@@ -400,7 +400,7 @@ class PointSource(Source, shield.Shield):
         kwargs['material_name'] = 'air'
         kwargs['density'] = 0
         super().__init__(**kwargs)
-        self.points_per_dimension = [1]
+        self.points_per_dimension = [1, 1, 1]
 
     def is_infinite(self):
         """Returns true if any dimension is infinite, false otherwise
@@ -409,7 +409,7 @@ class PointSource(Source, shield.Shield):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.
@@ -548,7 +548,7 @@ class BoxSource(Source, shield.Box):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.
@@ -590,7 +590,7 @@ class ZAlignedCylinderSource(Source, shield.ZAlignedCylinder):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.
@@ -659,7 +659,7 @@ class YAlignedCylinderSource(Source, shield.YAlignedCylinder):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.
@@ -728,7 +728,7 @@ class XAlignedCylinderSource(Source, shield.XAlignedCylinder):
 
     def _get_source_point_weights(self):
         return [1.0 / np.product(self.points_per_dimension)] * \
-            np.sum(self.points_per_dimension)
+            np.product(self.points_per_dimension)
 
     def _get_source_points(self):
         """Generates a list of point sources within the Source geometry.